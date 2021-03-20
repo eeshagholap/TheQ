49d49057ac9957ce54306881a00aa0258d7e99a1
wangandreww <59212728+wangandreww@users.noreply.github.com>
2021-03-20 19:24:08 -0400

    Updated Form and app.py

--- theq_app/api/api.py
+++ theq_app/api/api.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask,request
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import os 
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/api', methods=['GET', 'POST'])
 def api():
+    if request.method == 'POST':
+        print(request.form)
+        print(request.form['name'])
     return{
         'userid:': 1,
         'title': "Test Flask app",
         'Completed': False
     }
+
+
