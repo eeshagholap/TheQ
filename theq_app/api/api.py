95e35f68d9c68628b73cd693b46747a4f352c509
Eesha <gholap00@gmail.com>
2021-03-21 11:31:31 -0400

    sending form entry to the database fixed

--- theq_app/api/api.py
+++ theq_app/api/api.py
@@ -2,7 +2,6 @@ from flask import Flask, request, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 from flask import g
-from show_all import show_all
 
 app = Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///theq.db'
@@ -22,23 +21,23 @@ db = SQLAlchemy(app)
 
 ### Student input fields
 
-class students(db.Model):
+class test(db.Model):
     id = db.Column('id', db.Integer, primary_key = True)
     name = db.Column(db.String(1000))
     question = db.Column(db.String(10000))  
     zoom_link = db.Column(db.String(1000))
-    place_in_queue = db.Column(db.Integer)
+    #place_in_queue = db.Column(db.Integer)
         
-    def __init__(self, name, question, zoom_link,place_in_queue):
+    def __init__(self, name, question, zoom_link):#,place_in_queue):
         self.name = name
         self.question = question
         self.zoom_link = zoom_link
-        self.place_in_queue = place_in_queue
+        #self.place_in_queue = place_in_queue
 
 
 @app.route('/')
 def show_all():
-   return render_template('show_all.html', students = students.query.all())
+   return render_template('show_all.html', test = test.query.all())
 
 @app.route('/new', methods = ['GET', 'POST'])
 def new():
@@ -46,8 +45,8 @@ def new():
       if not request.form['name'] or not request.form['question'] or not request.form['zoom_link']:
          flash('Please enter all the fields', 'error')
       else:
-         student = students(request.form['name'], request.form['question'],
-            request.form['zoom_link'], request.form['place_in_queue'])
+         student = test(request.form['name'], request.form['question'],
+            request.form['zoom_link'])#, request.form['place_in_queue'])
          
          db.session.add(student)
          db.session.commit()
