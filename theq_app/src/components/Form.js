7322fa464d78ea74f775a8f94853110bc2536924
wangandreww <59212728+wangandreww@users.noreply.github.com>
2021-03-24 12:36:08 -0400

    Updated Form and its css file.

--- theq_app/src/components/Form.js
+++ theq_app/src/components/Form.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React from 'react'
+import './form.css'
+import photo from './formpic.jpg';
+class MyForm extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    fetch('/api', {
+      method: 'POST',
+      body: (data),
+    });
+    document.getElementById("queueForm").reset();
+  }
 
-const Form = () => {
+  render() {
     return (
-        <div className="wrapper">
-            <h1>Enter the Queue</h1>
-            <form>
-                <fieldset>
-                    <label>
-                        <p>Name</p>
-                        <input name="name" />
-                    </label>
-                </fieldset>
-                <fieldset>
-                    <label>
-                        <p>Question</p>
-                        <input name="question" />
-                    </label>
-                </fieldset>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+      <div>
+      
+      <h1 className = "formHeader"> Get on TheQ </h1>
+      <img className = "formphoto" src = {photo} alt = "" />
+     
+
+      <form id="queueForm" onSubmit={this.handleSubmit}>
+        <label className = "namelabel" htmlFor="name">Name</label>
+        <input className = "nameinput" name="name" type="Name" />
+
+        <label className = "questionlabel" htmlFor="question">Question</label>
+        <input className = "questioninput" id="question" name="question" type="question" />
+
+        <label className = "linklabel" htmlFor="link">Zoom Link</label>
+        <input className = "linkinput" id="link" name="link" type="link" />
+
+        <button className = "formButton"> Join TheQ </button>
+
+        
+      </form>
+
+      </div>
     );
+  }
 }
-
-
-
 
 
 /*
@@ -47,4 +65,4 @@ function App() {
         </a>
       </header>
 */
-export default Form;
+export default MyForm;
