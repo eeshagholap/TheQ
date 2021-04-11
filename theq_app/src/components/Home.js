c85efda5532aee3055ba945756bc2c71b4ec2c54
Amy Feng <afeng@Amys-MacBook-Air.local>
2021-04-11 11:46:20 -0600

    Updated home page with new designs

--- theq_app/src/components/Home.js
+++ theq_app/src/components/Home.js
@@ -10,7 +10,79 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
             <div>
+
                 <view style = {{top: 42, position: 'absolute', marginLeft: 55, width:127, height: 91}}>
+                <img src = {logo} alt = "logo"></img>
+                </view>
+
+                <view style = {{position: 'absolute', width:1000, height: 940, marginLeft: 70 + 'px', top: 120,}}>
+                <img src = {img1} alt = "img1"></img>
+                </view>
+
+                <view style = {{position: 'absolute', width: 400 + 'px', height: 200 + 'px', left: 800 + 'px', 
+                        top: 60 + 'px', bottom: 1140 + 'px', right: 193 + 'px', 
+                        fontFamily: 'Palanquin Dark', fontStyle: 'normal', fontWeight: 600,
+                        fontSize: 24, lineHeight: 0, color: "black",  marginTop:0}}>
+                <p>Are you an instructor or student?</p>
+                </view>
+
+                <Link to={'/queue'}>
+                    <button className = "instructorsButton"> Instructors </button>      
+                </Link>
+                
+                <Link to={'/form-submission'}>
+                    <button className = "studentsButton">Students</button>
+                </Link>
+
+                <view style = {{position: 'absolute', width:284, height: 86, marginLeft: 830, top: 220}}>
+                <img src = {getonq} alt = "getonq"></img>
+                </view>
+
+                <view style = {{position: 'absolute', width: 400 + 'px', height: 200 + 'px', left: 670 + 'px', 
+                    top: 280 + 'px', bottom: 1140 + 'px', right: 193 + 'px', 
+                    fontFamily: 'Palanquin Dark', fontStyle: 'normal', fontWeight: 600,
+                    fontSize: 24, lineHeight: 0, color: "lightgreen",  marginTop:0}}>
+                <p>Fast, Reliable, Efficient</p>
+                </view>
+
+                <view style = {{position: 'absolute', width: 300 + 'px', height: 500 + 'px', left: 670 + 'px', 
+                        top: 330 + 'px', bottom: 500 + 'px', right: 193 + 'px', 
+                        fontFamily: 'Palanquin Dark', fontStyle: 'normal', fontWeight: 600,
+                        fontSize: 18, lineHeight: 0, color: "black",  marginTop:0, wordWrap: "normal"}}>
+                <p>Our queue system will make the</p>
+                <p>office hours experience quicker</p>
+                <p>and more feasible to navigate. It's</p>
+                <p>time to take charge of </p>
+                <p>online learning.</p>
+                </view>
+
+                <view style = {{position: 'absolute', width:10, height: 100, marginLeft: 1020, top: 300,}}>
+                <img src = {img2} alt = "img2"></img>
+                </view>
+
+                <view style = {{position: 'absolute', width: 400 + 'px', height: 200 + 'px', left: 970 + 'px', 
+                        top: 450 + 'px', bottom: 1140 + 'px', right: 193 + 'px', 
+                        fontFamily: 'Palanquin Dark', fontStyle: 'normal', fontWeight: 600,
+                        fontSize: 28, lineHeight: 0, color: "lightgreen",  marginTop:0}}>
+                <p>Collaborative and Fun</p>
+                </view>
+                
+                <view style = {{position: 'absolute', width:284, height: 100, marginLeft: 690, top: 450,}}>
+                <img src = {img3} alt = "img3"></img>
+                </view>
+
+                <view style = {{position: 'absolute', width: 300 + 'px', height: 500 + 'px', left: 980 + 'px', 
+                        top: 500 + 'px', bottom: 500 + 'px', right: 193 + 'px', 
+                        fontFamily: 'Palanquin Dark', fontStyle: 'normal', fontWeight: 600,
+                        fontSize: 18, lineHeight: 0, color: "black",  marginTop:0, wordWrap: "normal"}}>
+                <p>Our queue system will make the</p>
+                <p>office hours experience quicker</p>
+                <p>and more feasible to navigate. It's</p>
+                <p>time to take charge of </p>
+                <p>online learning.</p>
+                </view>
+
+                {/*<view style = {{top: 42, position: 'absolute', marginLeft: 55, width:127, height: 91}}>
                 <img src = {logo} alt = "logo"></img>
                 </view>
                 
@@ -81,12 +153,12 @@ const Home = () => {
                     </view>
                 </div>
 
-                     <Link to={'/queue'}>
+                    <Link to={'/queue'}>
                         <button className = "instructorsButton"> Instructors </button>      
                     </Link>
                     <Link to={'/form-submission'}>
                         <button className = "studentsButton">Students</button>
-                    </Link>
+                    </Link>*/}
                     
             </div>
         );
